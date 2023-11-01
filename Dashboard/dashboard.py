b46cc734d2aad4f3360fa6f95743c4377f9c30aa
Tuti Amalia <44547435+TutiAmalia@users.noreply.github.com>
2023-11-01 15:02:07 +0800

    tambahan

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
-import plotly.graph_objects as go
 
 st.set_page_config(page_title="E-commerce Insights")
 
---
+++ README.md
@@ -0,0 +1,12 @@
+# Dicoding Collection Dashboard ✨
+
+## Setup environment
+```
+
+```
+
+## Run streamlit app
+```
+cd dashboard
+streamlit run dashboard.py
+```
